b56b05558faaf101925c7a7bfde6ea41a618d6b1
Hollujay <165713167+Hollujay@users.noreply.github.com>
2026-07-20 18:50:22 +0000

    style: fix formatting

--- contracts/src/savings/events.rs
+++ contracts/src/savings/events.rs
@@ -1,8 +1,6 @@
 use soroban_sdk::{symbol_short, Address, Env};
 
 pub fn emit_savings_deposited(env: &Env, user: &Address, amount: i128) {
-    env.events().publish(
-        (symbol_short!("SavDep"), user.clone()),
-        amount,
-    );
+    env.events()
+        .publish((symbol_short!("SavDep"), user.clone()), amount);
 }
